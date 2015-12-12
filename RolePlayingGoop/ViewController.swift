44ee51c0f8399a05bf0df0eda6549f49460c670c
Felix Barros <felix.barros@gmail.com>
2015-12-12 17:06:29 -0500

    Connect outlets to storyboard elements and initialize player and enemy

--- RolePlayingGoop/ViewController.swift
+++ RolePlayingGoop/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLabel: UILabel!
+    @IBOutlet weak var playerHealthPoints: UILabel!
+    @IBOutlet weak var enemyHealthPoints: UILabel!
+    @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestButton: UIButton!
+
+    var player: Player!
+    var enemy: Enemy!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "Dirty Laundry", startingHealthPoints: 110, startingAttackPower: 20)
+        
+        generateRandomEnemy()
+
+        updateHealthPoints()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Helper functions
+    func generateRandomEnemy() {
+        let randomNumber = Int(arc4random_uniform(UInt32(2)))
+        
+        switch randomNumber {
+        case 1:
+            enemy = Kimara(startingHealthPoints: 50, startingAttackPower: 12)
+        default:
+            enemy = FiendishWizard(startingHealthPoints: 60, startingAttackPower: 15)
+        }
     }
-
+    func updateHealthPoints() {
+        playerHealthPoints.text = "\(player.healthPoints) HP"
+    }
 
 }
 
